61d9da11f18b8a4d3af3429811d433a8e029b528
zamora-wni <zamora-wni@users.noreply.github.com>
2026-04-24 15:01:33 +0900

    Update CUDA architecture version in setup.py
    Removed line gencode compute 70

--- grounding_dino/setup.py
+++ grounding_dino/setup.py
@@ -92,7 +92,6 @@ def get_extensions():
             "-D__CUDA_NO_HALF_OPERATORS__",
             "-D__CUDA_NO_HALF_CONVERSIONS__",
             "-D__CUDA_NO_HALF2_OPERATORS__",
-            "-gencode=arch=compute_70,code=sm_70",
             "-gencode=arch=compute_75,code=sm_75",
             "-gencode=arch=compute_80,code=sm_80",
             "-gencode=arch=compute_86,code=sm_86",
